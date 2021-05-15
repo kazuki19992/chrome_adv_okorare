3f5b5e5878fd4f6008ecc4e549c26e1ea9884e48
kazuki19992 <kushida98@gmail.com>
2021-05-15 23:41:23 +0900

    いいかんじ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [block, updateBlock] = useState('')
+  let block = ''
+
+  const urlParamStr = window.location.search.substring(1)
+  console.log(urlParamStr)
+
+  if(urlParamStr.length){
+    let params = {}
+
+    // クエリパラメータを取得する
+    urlParamStr.split('&').forEach( param => {
+      const tmp = param.split('=')
+      params = {
+        ...params,
+        [tmp[0]]: tmp[1]
+      }
+    })
+    console.log(params.block)
+
+    block = params.block
+  }
+
+  if(block === ''){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            何しに来たんですか
+          </p>
+          <a
+            className="App-link"
+            href="https://Twitter.com/Tech_Kazu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            開発者Twitter
+          </a>
+        </header>
+      </div>
+    );
+  }else{
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            お前がChromeちゃんを差し置いて<code>{block}</code>ばかり見ているので<br />
+            Chromeちゃんが怒って<code>{block}</code>をブロックしちゃいました。
+          </p>
+          <p>お前のせいです。あーあ。</p>
+          <a
+            className="App-link"
+            href="https://Twitter.com/Tech_Kazu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            開発者に抗議する
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
